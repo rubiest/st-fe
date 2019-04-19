09e1723ed3c8f70815a2a6bdb21ab0581100538f
AmirolAhmad <dev.amirol@gmail.com>
2019-04-20 07:18:24 +0800

    scaffold the header navbar with react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header'
+import Home from './Home';
+import About from './About';
 
 const App = () => {
   return(
-    <Header />
+    <div>
+      <BrowserRouter>
+        <Header />
+        <div class="container-fluid">
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
